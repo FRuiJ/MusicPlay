2cf1f4ac34d8d0ebb44518a750e03742aaf467f3
FRuiJ <1844378892@qq.com>
2021-06-22 13:42:33 +0800

    2

--- LocalMusic/app/src/main/java/com/example/localmusic/LocalMusicAdapter.java
+++ LocalMusic/app/src/main/java/com/example/localmusic/LocalMusicAdapter.java
@@ -1,6 +1,59 @@
 package com.example.localmusic;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class LocalMusicAdapter implements RecyclerView.Adapter<> {
+import java.util.List;
+
+public class LocalMusicAdapter extends RecyclerView.Adapter<LocalMusicAdapter.LocalMusicViewHolder> {
+
+    Context context;
+    List<LocalMusicBean> mDatas;
+
+    public LocalMusicAdapter(Context context, List<LocalMusicBean> mDatas) {
+        this.context = context;
+        this.mDatas = mDatas;
+    }
+
+    @NonNull
+    @Override
+    public LocalMusicViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_local_music, parent, false);
+        LocalMusicViewHolder holder = new LocalMusicViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LocalMusicAdapter.LocalMusicViewHolder holder, int position) {
+        LocalMusicBean musicBean = mDatas.get(position);
+        holder.idTv.setText(musicBean.getId());
+        holder.songTv.setText(musicBean.getSong());
+        holder.singerTv.setText(musicBean.getSinger());
+        holder.albumTv.setText(musicBean.getAlbum());
+        holder.timeTv.setText(musicBean.getTime());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    class LocalMusicViewHolder extends RecyclerView.ViewHolder {
+        TextView idTv, songTv, singerTv, albumTv, timeTv;
+
+        public LocalMusicViewHolder(@NonNull View itemView) {
+            super(itemView);
+            idTv = itemView.findViewById(R.id.item_tv_num);
+            songTv = itemView.findViewById(R.id.item_tv_song);
+            singerTv = itemView.findViewById(R.id.item_tv_singer);
+            albumTv = itemView.findViewById(R.id.item_tv_album);
+            timeTv = itemView.findViewById(R.id.item_tv_time);
+        }
+    }
 }
